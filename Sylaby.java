bce9528e6e3b9d862f9e689642f4d3225502a6d7
javam140 <81302662+javam140@users.noreply.github.com>
2022-09-29 18:57:03 +0000

    pierwszy commit

---
+++ Sylaby.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+public class Sylaby {
+    static public int iloscsylab;
+    static void wypisz(String x) {
+        System.out.println(x);
+    }
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        wypisz("wprowadz tekst");
+        String tekst = in.nextLine();
+        String[] tablica = tekst.split(" ");
+        int i = 0;
+        while (i<=tablica.length-1) {
+            String slowo = tablica[i];
+            int ilosc_a = 0;
+            char tym;
+            int ilosc_e = 0;
+            int ilosc_i = 0;
+            int ilosc_o = 0;
+            int ilosc_u = 0;
+            int ilosc_y = 0;
+
+            for (int j=0;j<slowo.length(); j++) {
+                tym = slowo.charAt(j);
+                if (tym=='a') {
+                    ilosc_a++;
+                }
+                if (tym=='e') {
+                    ilosc_e++;
+                }
+                if (tym=='i') {
+                    ilosc_i++;
+                }
+                if (tym=='o') {
+                    ilosc_o++;
+                }
+                if (tym=='u') {
+                    ilosc_u++;
+                }
+                if (tym=='y') {
+                    ilosc_y++;
+                }
+            }
+            iloscsylab += ilosc_a+ilosc_e+ilosc_i+ilosc_o+ilosc_u+ilosc_y;
+            i++;
+        }
+        wypisz("ilosc sylab w tekscie to "+iloscsylab);
+        in.close();
+    }
+}
